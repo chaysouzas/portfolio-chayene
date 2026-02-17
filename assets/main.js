ec1c357c83c11e909eb3068b1d465f65827f0189
Chayene <chayssilva16@gmail.com>
2026-02-16 22:20:55 -0300

    nova versão

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,62 @@
-// Versão simplificada
-const menuHamburguer = document.getElementById('menuHamburguer');
-const menuLista = document.getElementById('menuLista');
+ // JS REFATORADO
 
-menuHamburguer.addEventListener('click', function() {
-    menuLista.classList.toggle('active');
+ //THEMA
+
+const toggleButton = document.getElementById("themeToggle");
+const html = document.documentElement;
+
+function updateIcon(isLight) {
+  const iconName = isLight ? "sun" : "moon";
+  toggleButton.innerHTML = `<i data-lucide="${iconName}"></i>`;
+  lucide.createIcons();
+}
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+  html.classList.add("light");
+  updateIcon(true);
+  toggleButton.setAttribute("aria-pressed", "true");
+} else {
+  updateIcon(false);
+}
+
+toggleButton.addEventListener("click", () => {
+  html.classList.toggle("light");
+
+  const isLight = html.classList.contains("light");
+
+  updateIcon(isLight);
+  toggleButton.setAttribute("aria-pressed", isLight);
+
+  localStorage.setItem("theme", isLight ? "light" : "dark");
 });
 
-// Fechar menu ao clicar nos links
-const menuLinks = document.querySelectorAll('.cabecalho__menu-link');
-menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        menuLista.classList.remove('active');
+
+
+
+// ABOUT ME ANIMATION
+ const elements = document.querySelectorAll('.fade-up');
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('show');
+      }
     });
+  });
+
+  elements.forEach(el => observer.observe(el));
+
+//BOTÃO HAMBURGUER
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
 });
 
 
-// Controle de reprodução de vídeo 
 
-  const videoHeader = document.getElementById('headerVideo');
-  videoHeader.playbackRate = 0.8; // 
 
-  const videoQuemSouEu = document.getElementById('quemSouEu');
-  videoQuemSouEu.playbackRate = 0.3; // 
 
